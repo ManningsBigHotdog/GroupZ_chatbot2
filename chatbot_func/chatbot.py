a586c409839b3d4a439a51dfdfc974dc36604e3f
23473355_TimTong <23473355@life.hkbu.edu.hk>
2024-03-30 17:06:36 +0800

    functions_text bug fix

--- chatbot_func/chatbot.py
+++ chatbot_func/chatbot.py
@@ -22,7 +22,7 @@ def button(update: Update, context: CallbackContext):
                 "- /start - Start the bot and show this message.\n"
                 "- /search - Search for comments.\n"
                 "- /addcity - Add a new city comment.\n"
-                "- /plantrip - Help you to plan a trip of the city your want to go"
+                "- /plantrip - Help you to plan a trip of the city your want to go\n"
                 "- Normal sending message - ChatGPT function"
             )
             query.edit_message_text(text=functions_text)
